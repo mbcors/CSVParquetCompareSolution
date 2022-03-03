3c00f2bb205e4a6af2608b4fa9b28fb37ea95f1e
Marius Badana <marius.badana@gmail.com>
2022-03-04 01:33:51 +0200

    Add project files.

---
+++ CSVParquetCompare/Comparer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Parquet;
+
+namespace CSVParquetCompare
+{
+    public static class Comparer
+    {
+        public static List<string> CSVDataReader(string filename)
+        {
+            List<string> csvList = new List<string>();
+            using (var rd = new StreamReader(filename))
+            {
+                while (!rd.EndOfStream)
+                    csvList.Add(rd.ReadLine());
+            }
+            return csvList;
+        }
+
+        public static List<string> ParquetDataReader(string filename)
+        {
+
+            List<string> parquetList = new List<string>();
+            MemoryStream ParquetData = new MemoryStream();
+            using (FileStream source = File.Open(filename, FileMode.Open))
+                source.CopyTo(ParquetData);
+            using (ParquetReader pr = new ParquetReader(ParquetData))
+            {
+                var table = pr.ReadAsTable();
+                foreach (var row in table)
+                {
+                    var response = JsonConvert.DeserializeObject<ParquetResponse>(row.ToString());
+                    parquetList.Add(response.Domain);
+                }
+            }
+            return parquetList;
+        }
+    }
+
+    public class ParquetResponse
+    {
+        public string Domain { get; set; }
+    }
+}
---
+++ CSVParquetCompare/Program.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVParquetCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> csvFilesList = new List<string>(Directory.GetFiles(Directory.GetCurrentDirectory() + "\\csv"));
+            List<string> parquetFilesList = new List<string>(Directory.GetFiles(Directory.GetCurrentDirectory() + "\\parquet"));
+            if (csvFilesList.Count != parquetFilesList.Count)
+            {
+                Console.WriteLine("Number of csv files (" + csvFilesList.Count + ") does not match the parquet files (" + parquetFilesList.Count + ")");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            for (int i = 0; (i < csvFilesList.Count); i++)
+            {
+                try
+                {
+                    var csvFile = Comparer.CSVDataReader(csvFilesList[i]);
+                    var parquetFile = Comparer.ParquetDataReader(parquetFilesList[i]);
+                    CollectionAssert.AreEqual(csvFile, parquetFile);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("PASS");
+                    Console.ResetColor();
+                    Console.WriteLine((" - " + Path.GetFileName(csvFilesList[i])) + " & " + Path.GetFileName(parquetFilesList[i]));
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("FAIL");
+                    Console.ResetColor();
+                    Console.WriteLine(" - " + Path.GetFileName(csvFilesList[i]) + " & " + Path.GetFileName(parquetFilesList[i]));
+                    Console.WriteLine(e.Message.ToString());
+
+                }
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+    }
+}
