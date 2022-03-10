a709e64e5ccd43d1bcfa5659f4647e0524a7aba9
Marius Badana <marius.badana@gmail.com>
2022-03-10 13:14:22 +0200

    added comment

--- CSVParquetCompare/Program.cs
+++ CSVParquetCompare/Program.cs
@@ -6,7 +6,11 @@ using System.Reflection;
 namespace CSVParquetCompare
 {
     class Program
-    {
+    {/// <summary>
+    /// Move data.zip to \Data folder
+    /// Change the location parameter as required (have csv/parquet structure inside)
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
             Compare.CsvParquet("csv_parquet");
